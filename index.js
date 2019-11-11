6a84127c7afda0550a14ebf6a703c405e50b5bed
Angela Flowers <me@angela.flowers>
2019-11-11 12:37:45 -0500

    commit

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const server = require('./api/server.js');
 
+const bcrypt = require('bcryptjs')
+
+server.use(bcrypt());
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`\n** Running on port: ${PORT} **\n`));
